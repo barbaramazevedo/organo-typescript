f7ba61b50b061e18de028c725459a8cf060c28c1
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-06-01 20:15:45 -0300

    feat: refactoring form to typescript

--- src/componentes/Botao/index.tsx
+++ src/componentes/Botao/index.tsx
@@ -1,15 +1,13 @@
-import { ReactElement } from 'react'
-import './Botao.css'
-import React from 'react';
+import { ReactElement } from "react";
+import "./Botao.css";
+import React from "react";
 
 interface BotaoProps {
-    children: ReactElement
+  children: ReactElement | string;
 }
 
 const Botao = (props: BotaoProps) => {
-    return (<button className='botao'>
-        {props.children}
-    </button>)
-}
+  return <button className="botao">{props.children}</button>;
+};
 
-export default Botao
+export default Botao;
